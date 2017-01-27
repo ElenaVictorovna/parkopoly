b5eb14c192739d1721d31ab0dd3ca39f3284e067
ed <edick.makukhin@gmail.com>
2017-01-27 17:23:52 +0200

    feat(footer) add dialog

--- src/app/components/pageFooter/pageFooter.directive.js
+++ src/app/components/pageFooter/pageFooter.directive.js
@@ -3,17 +3,50 @@
 
   angular
     .module('parkopoly')
-    .directive('pageFooter', pageFooter);
+    .directive('pageFooter', ['$mdDialog', pageFooter]);
 
   /** @ngInject */
   pageFooter.$inject = [];
 
   function pageFooter() {
     /** @ngInject */
-    function PageFooterController() {
+    function PageFooterController( $mdDialog ) {
 
       var vm = this;
 
+      vm.showDialog = function($event) {
+        var parentEl = angular.element(document.body);
+
+        $mdDialog.show({
+          parent: parentEl,
+          targetEvent: $event,
+          template:
+          '<md-dialog aria-label="List dialog">' +
+          '  <md-dialog-content>'+
+          '    <md-list>'+
+          '      <md-list-item>'+
+          '       <p>Hello World !</p>' +
+          '     </md-list-item>' +
+          '    </md-list>'+
+          '  </md-dialog-content>' +
+          '  <md-dialog-actions>' +
+          '    <md-button ng-click="dialog.closeDialog()" class="md-primary">' +
+          '      Close Dialog' +
+          '    </md-button>' +
+          '  </md-dialog-actions>' +
+          '</md-dialog>',
+          locals: {
+            items: vm.items
+          },
+          controllerAs: 'dialog',
+          controller: function test($scope, $mdDialog, items) {
+            vm.items = items;
+            this.closeDialog = function() {
+              $mdDialog.hide();
+            };
+          }
+        });
+      };
 
     }
 
